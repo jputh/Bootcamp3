ea87b4693d677b85a75dc38c1e83da9403c2fd72
jputh <36486225+jputh@users.noreply.github.com>
2019-10-02 22:13:22 -0400

    remove config file

--- server/config/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  db: {
-    uri: 'mongodb+srv://user1:directoryaccess@directorydata-rxnn0.mongodb.net/test?retryWrites=true&w=majority'
-  }, 
-  openCage: {
-    key: '6a1d1e57d4ba4cc9a6309d8277f0ed46'
-  },
-  port: 8080
-};
